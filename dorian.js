ad9f21d55d27194ae40de7a816df44ca271b21c5
Aaron Dayalan <adayalan@shutterstock.com>
2015-05-01 22:03:50 -0400

    wip

--- dorian.js
+++ dorian.js
@@ -22,12 +22,19 @@ function testFn(exported_object) {
 
         var test_matrix = cmbx.baseN(arg_test_values, fn_args.length).toArray();
 
-        test_matrix.forEach(function (fn_args) {
+        if (test_matrix.length > 0) {
+            test_matrix.forEach(function (fn_args) {
+                mocha.addTest(fn_declaration[0] + ' - can handle - ' + JSON.stringify(fn_args), function (done) {
+                    expect(exported_object.apply(this, fn_args)).to.not.throw;
+                    done();
+                });
+            });
+        } else {
             mocha.addTest(fn_declaration[0] + ' - can handle - ' + JSON.stringify(fn_args), function (done) {
-                expect(exported_object.apply(this, fn_args)).to.not.throw;
+                expect(exported_object.apply(this).to.not.throw;
                 done();
             });
-        });
+        }
 
     }
 }
@@ -47,26 +54,31 @@ internal_modules.forEach(function(app_module){
             //inspect fn args
             testFn(exported_object);
             break;
-        case 'Object':
-            exported_object.forEach(function(module_prop){
-                switch(typeof module_prop) {
-                    case 'function':
-                        testFn(module_prop);
-                        break;
-                    // TODO: lets get recursive.
+        case 'object':
+            for(var prop in exported_object){
+                if (exported_object.hasOwnProperty(prop)) {
+                    var module_prop = exported_object[prop];
+                    switch (typeof module_prop) {
+                        case 'function':
+                            testFn(module_prop);
+                            break;
+                        // TODO: lets get recursive.
+                    }
                 }
-            });
+            }
             break;
         default:
             console.log('Unsupported module.exports type');
             break;
-    };
-    mocha.run(function(failures){
-        if(failures) {
-            console.log("Failures: %s", failures);
-        } else {
-            console.log("Module tests complete");
-        }
-    });
+    }
+
+
 });
 
+mocha.run(function(failures) {
+    if (failures) {
+        console.log("Failures: %s", failures);
+    } else {
+        console.log("Module tests complete");
+    }
+});
