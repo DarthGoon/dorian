ff927808d07469a720c0abc55252ea345bb53813
Aaron Dayalan <adayalan@shutterstock.com>
2015-05-02 00:55:57 -0400

    tests, tests, tests!!  need to gaurd against callback failures

--- dorian.js
+++ dorian.js
@@ -14,12 +14,30 @@ var third_party_modules = [];
 var internal_modules = [];
 var arg_test_values = [{}, null, ''];
 
+var callback_fn = function(err, data){
+    expect(typeof err).to.not.eq('error');
+    console.log('callback called');
+};
+
+function generateMatrix(fn_args){
+    var callback_arg_position = fn_args.indexOf('callback');
+    var args_count = callback_arg_position == -1 ? fn_args.length : fn_args.length - 1;
+
+    var matrix = cmbx.baseN(arg_test_values, args_count).toArray();
+
+    if (callback_arg_position != -1) {
+        matrix.forEach(function (args) {
+            args.splice(callback_arg_position, 0, callback_fn);
+        });
+    }
+    return matrix;
+}
+
 function testFn(exported_object) {
-    var fn_declaration = /(?:function \()([^\)]+)\)/g.exec(exported_object.toString());
+    var fn_declaration = /(?:function\s\w+\(|function\s\()([^\)]+|)\)/g.exec(exported_object.toString());
     if (fn_declaration) {
         var fn_args = fn_declaration[1].replace(' ', '').split(',');
-
-        var test_matrix = cmbx.baseN(arg_test_values, fn_args.length).toArray();
+        var test_matrix = generateMatrix(fn_args);
 
         if (test_matrix.length > 0) {
             test_matrix.forEach(function (fn_args) {
@@ -38,6 +56,31 @@ function testFn(exported_object) {
     }
 }
 
+function walkTheTree(exported_object){
+    switch (typeof exported_object){
+        case 'function':
+            //inspect fn args
+            testFn(exported_object);
+            break;
+        case 'object':
+            for(var prop in exported_object){
+                if (exported_object.hasOwnProperty(prop)) {
+                    var module_prop = exported_object[prop];
+                    walkTheTree(module_prop);
+                }
+            }
+            break;
+        case 'string':
+        case 'number':
+        case 'boolean':
+            console.log('Skipping exported primitive- %s:%s', typeof exported_object, exported_object);
+            break;
+        default:
+            console.log('Unsupported module.exports type: %s', typeof exported_object);
+            break;
+    }
+}
+
 module.parent.children.forEach(function(module){
     if (module.filename.indexOf('node_modules') != -1) {
         third_party_modules.push(module);
@@ -48,30 +91,7 @@ module.parent.children.forEach(function(module){
 
 internal_modules.forEach(function(app_module){
     var exported_object = app_module.exports;
-    switch (typeof exported_object){
-        case 'function':
-            //inspect fn args
-            testFn(exported_object);
-            break;
-        case 'object':
-            for(var prop in exported_object){
-                if (exported_object.hasOwnProperty(prop)) {
-                    var module_prop = exported_object[prop];
-                    switch (typeof module_prop) {
-                        case 'function':
-                            testFn(module_prop);
-                            break;
-                        // TODO: lets get recursive.
-                    }
-                }
-            }
-            break;
-        default:
-            console.log('Unsupported module.exports type');
-            break;
-    }
-
-
+    walkTheTree(exported_object);
 });
 
 mocha.run(function(failures) {
