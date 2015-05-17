fb9083cf76acdce4d622d93fc5b0c7f41b250252
Aaron Dayalan <adayalan@shutterstock.com>
2015-05-17 12:58:53 -0400

    wip auto-coverage

--- dorian.js
+++ dorian.js
@@ -1,12 +1,20 @@
+var fs = require('fs');
 var cmbx = require('./lib/combinatorics').Combinatorics;
 var mocha_module = require('mocha');
 var expect = require('chai').expect;
+var istanbul = require('istanbul');
+var instrumenter = new istanbul.Instrumenter();
+var reporter = istanbul.Report.create('html');
+var collector = new istanbul.Collector;
 
 var mocha = new mocha_module({
     ui: 'tdd',
     reporter: 'spec'
 });
 
+reporter.on('done', function () {
+    console.log('Wrote coverage Report');
+});
 
 
 /**
@@ -20,15 +28,16 @@ var function_whitelist = [];
 /**
  * TODO: this feels weird, you can name a callback anything.
  * Should figure out how to detect the param as a function.
-  */
+ */
 var callback_whitelist = ['callback', 'next', 'cb'];
 
 
 
 var third_party_modules = [];
 var internal_modules = [];
-var arg_test_values = [{}, null, '', 0, 1, -1];
+var arg_test_values = [null];
 var fn_slicer = /(?:function\s\w+\(|function\s\()([^\)]+)\)/;
+var fn_name_slicer = /(?:function\s)(\w+)/;
 
 function callback_fn(done) {
     return function () {
@@ -70,9 +79,9 @@ function testFn(exported_object) {
     var mochaTest = mocha_module.Test;
     var fn_declaration = fn_slicer.exec(exported_object.toString());
     if (fn_declaration) {
-        var fn_args = fn_declaration[1].replace(/\s/g,'').split(',');
-        var fn_name = /(?:function\s)(\w+)/.exec(fn_declaration[0]);
-        if (fn_name && function_blacklist.indexOf(fn_name[1]) != -1){
+        var fn_args = fn_declaration[1].replace(/\s/g, '').split(',');
+        var fn_name = fn_name_slicer.exec(fn_declaration[0]);
+        if (fn_name && function_blacklist.indexOf(fn_name[1]) != -1) {
             return;
         }
         var test_matrix = generateMatrix(fn_args);
@@ -83,14 +92,14 @@ function testFn(exported_object) {
             test_matrix.values.forEach(function (fn_args) {
                 mocha.suite.addTest(new mochaTest(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
                     var test_wired_args;
-                    if (hasCallback){
+                    if (hasCallback) {
                         test_wired_args = fill_callback_fn({
                             matrix: fn_args,
                             callback_arg_position: callback_arg_position
                         }, done)
                     }
                     expect(exported_object.apply(this, test_wired_args)).to.not.throw;
-                    if (!hasCallback){
+                    if (!hasCallback) {
                         done();
                     }
                 }));
@@ -98,8 +107,8 @@ function testFn(exported_object) {
         } else {
             mocha.suite.addTest(new mochaTest(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
                 var test_wired_args = [];
-                if (hasCallback){
-                    test_wired_args = [ callback_fn(done) ];
+                if (hasCallback) {
+                    test_wired_args = [callback_fn(done)];
                 }
                 expect(exported_object.apply(this, test_wired_args)).to.not.throw;
                 if (!hasCallback) {
@@ -138,9 +147,13 @@ module.parent.children.forEach(function(module){
     if (module.filename.indexOf('node_modules') != -1) {
         third_party_modules.push(module);
     } else {
+        var instrumented_code = fs.readFileSync(module.filename);
+        instrumenter.instrumentSync(instrumented_code.toString(), module.filename);
+        collector.add(JSON.parse(instrumented_code.toString()));
         internal_modules.push(module);
     }
 });
+
 
 internal_modules.forEach(function(app_module){
     var exported_object = app_module.exports;
@@ -150,6 +163,7 @@ internal_modules.forEach(function(app_module){
 console.log('Tests generated: %s', mocha.suite.tests.length);
 console.log('Starting Mocha test run...');
 mocha.run(function(failures) {
+    reporter.writeReport(collector);
     console.log('Tests ran: %s', mocha.suite.tests.length);
     console.log('Failures: %s', failures || 0);
     console.log('Dorian test run success');
