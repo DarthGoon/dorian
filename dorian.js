5ae335b267d53614c8f5ddb364551a685199b16d
Aaron Dayalan <adayalan@shutterstock.com>
2015-10-05 10:52:45 -0400

    WIP- refactoring for memory leak.  still going though

--- dorian.js
+++ dorian.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var async = require('async');
 var cmbx = require('./lib/combinatorics').Combinatorics;
 var mocha_module = require('mocha');
-var expect = require('chai').expect;
+var assert = require('chai').assert;
 var istanbul = require('istanbul');
 var istanbul_core = require('./lib/istanbul-core');
 istanbul_core.hookLoader('/Users/adayalan/Engineering/opal', {
@@ -13,218 +14,224 @@ istanbul_core.hookLoader('/Users/adayalan/Engineering/opal', {
         };
     }
 });
-var mocha = new mocha_module({
-    ui: 'tdd',
-    reporter: 'spec'
-});
-var test_pass_incrementor = 0;
 
 /**
- * TODO: this started out for debugging.  But would probably
- * help to be able to target tests with params at runtime.
- **/
-var function_blacklist = [ 'doNotTestFunc', 'send_email_to_team', 'fraudCheck' ];
-var function_whitelist = [];
+ *  All of this is in the auto-cover branch, because i still haven't been able
+ *  to get a good coverage report generated.  This memory leak is just the latest
+ *  hurdle.  I'm gaining on this one though :)
 
-
-/**
- * TODO: this feels weird, you can name a callback anything.
- * Should figure out how to detect the param as a function.
+    So theres a memory leak.  Lets try to optimize execution and allocation.
+    No unnecessary frames.  Lightweight objects.  Serial test execution and generation.
+    Separate static and dynamic objects and funcs.
  */
-var callback_whitelist = ['callback', 'next', 'cb'];
 
-
-
-var third_party_modules = [];
-var internal_modules = [];
-var primitives = 0;
-var arg_test_values = [ {}, [], 0, 1 ];
-var fn_slicer = /(?:function\s\w+\(|function\s\()([^\)]+)\)/;
-var fn_name_slicer = /(?:function\s)(\w+)/;
-var multi_pass_generator = [];
-
-function callback_fn(done) {
-    return function () {
-        var assertion;
-        if (arguments) {
-            for (var idx = 0; idx < arguments.length; idx++) {
-                assertion = expect(typeof arguments[idx]).to.not.eq('error');
-                assertion && arguments[idx] && walkTheTree(arguments[idx]);
-            }
-        }
-        console.log('test callback fired');
-        done();
-    };
-}
-
-function fill_callback_fn(options, done) {
-    var matrix = options.matrix;
-    matrix.splice(options.callback_arg_position, 0, callback_fn(done));
-    return matrix;
-}
-
-function generateMatrix(fn_args){
-    var callback_arg_position = -1;
-    callback_whitelist.forEach(function(list_item){
-        if (callback_arg_position == -1) {
-            callback_arg_position = fn_args.indexOf(list_item);
-        }
-    });
-    var args_count = callback_arg_position < 0 ? fn_args.length : fn_args.length - 1;
-    var matrix = { values: args_count ? cmbx.baseN(arg_test_values, args_count).toArray() : [] };
-
-    if (callback_arg_position != -1) {
-        matrix.hasCallback = true;
-        matrix.callback_arg_position = callback_arg_position;
-        matrix.wireCallback = fill_callback_fn;
-    }
-    return matrix;
-}
-
-function testFn(exported_object) {
-    var mochaTest = mocha_module.Test;
-    var fn_declaration = fn_slicer.exec(exported_object.toString());
-    if (fn_declaration) {
-        var fn_args = fn_declaration[1].replace(/\s/g, '').split(',');
-        var fn_name = fn_name_slicer.exec(fn_declaration[0]);
-        if (fn_name && function_blacklist.indexOf(fn_name[1]) != -1) {
-            return;
-        }
-        var test_matrix = generateMatrix(fn_args);
-        var hasCallback = test_matrix.hasCallback;
-        var callback_arg_position = test_matrix.callback_arg_position;
-
-        if (test_matrix.values.length > 0) {
-            test_matrix.values.forEach(function (fn_args) {
-                mocha.suite.addTest(new mochaTest(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
-                    var test_wired_args,
-                        assertion;
-                    if (hasCallback) {
-                        test_wired_args = fill_callback_fn({
-                            matrix: fn_args,
-                            callback_arg_position: callback_arg_position
-                        }, done)
-                    }
-
-                    assertion = expect(exported_object.apply(this, test_wired_args)).to.not.throw;
-
-                    if (assertion && !hasCallback) {
-                        walkTheTree(exported_object.apply(this, test_wired_args));
-                        done();
-                    }
-                }));
-            });
-        } else {
-            mocha.suite.addTest(new mochaTest(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
-                var test_wired_args = [],
-                    assertion;
-                if (hasCallback) {
-                    test_wired_args = [callback_fn(done)];
-                }
-
-                assertion = expect(exported_object.apply(this, test_wired_args)).to.not.throw;
-
-                if (assertion && !hasCallback) {
-                    walkTheTree(exported_object.apply(this, test_wired_args));
-                    done();
-                }
-            }));
-        }
-    }
-}
-
-function walkTheTree(exported_object){
-    multi_pass_generator.push(function() {
+/**
+ * Static memory allocations
+ * Try to get as much of the constant stuff in here so it consolidates memory.
+ * Avoid any objects the grow, keep recursion in mind, move them to the workspace (below)
+ */
+var dorian_ops = {
+    mocha_instance: new mocha_module({ui: 'tdd', reporter: 'spec'}),
+    function_blacklist: ['send_email_to_team', 'fraudCheck'],
+    callback_whitelist: ['callback', 'next', 'cb'],
+    arg_test_values: [{}, [], 0, 1],
+    fn_slicer: /(?:function\s\w+\(|function\s\()([^\)]+)\)/,
+    fn_name_slicer: /(?:function\s)(\w+)/,
+    coverage_collector: istanbul.Collector(),
+    coverage_reporter: istanbul.Report,
+    walkTheTree: function walkTheTree(exported_object, parent_key) {
+        parent_key = parent_key || '';
         switch (typeof exported_object) {
             case 'function':
-                testFn(exported_object);
+                this.testFn(exported_object);
                 break;
             case 'object':
-                for (var prop in exported_object) {
-                    if (exported_object.hasOwnProperty(prop)) {
-                        var module_prop = exported_object[prop];
-                        module_prop && walkTheTree(module_prop);
-                    }
-                }
+                async.forEachOf(exported_object, function (val, key, callback) {
+                    val && console.log('Inspecting object: %s.%s', parent_key, key)
+                    this.walkTheTree(val, parent_key + '.' + key);
+                }, function (err) {
+                    err && console.error('ERROR: inspecting object tree', err)
+                        || console.log('Inspected object tree: %s', parent_key);
+                });
                 break;
             case 'string':
             case 'number':
             case 'boolean':
             default:
+                console.log('Skipping primitive prop: %s', parent_key);
                 break;
         }
-    });
-}
+    },
+    testFn: function testFn(exported_object) {
+        var fn_declaration, fn_args, fn_name, test_matrix;
+        fn_declaration = this.fn_slicer.exec(exported_object.toString());
+        if (fn_declaration) {
+            fn_args = fn_declaration[1].replace(/\s/g, '').split(',');
+            fn_name = this.fn_name_slicer.exec(fn_declaration[0]);
+            if (fn_name && this.function_blacklist.indexOf(fn_name[1]) != -1) {
+                return;
+            }
+            test_matrix = this.generateMatrix(fn_args);
 
-function buildTestSuite() {
-    var floor = multi_pass_generator.length;
-    if (test_pass_incrementor == 0 || multi_pass_generator.length < 100){
-        floor = 0;
-    } else {
-        floor = multi_pass_generator.length - 100;
-    }
-    console.log('Building new test suite');
-    while (multi_pass_generator.length > floor) {
-        multi_pass_generator.pop()();
-    }
-}
+            if (test_matrix.values.length > 0) {
+                async.each(test_matrix.values, function(fn_args){
+                    mocha.suite.addTest(new mocha_module.Test(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
+                        var test_wired_args,
+                            assertion;
+                        if (test_matrix.hasCallback) {
+                            test_wired_args = fill_callback_fn({
+                                matrix: fn_args,
+                                callback_arg_position: test_matrix.callback_arg_position
+                            }, done)
+                        }
 
-function seeWhatBreaks() {
-    var collector = new istanbul.Collector(),
-        Report = istanbul.Report;
+                        assertion = assert.doesNotThrow(exported_object.bind(this, test_wired_args), /.*/);
 
-    module.parent.children.forEach(function (module) {
-        if (module.filename.indexOf('node_modules') != -1
-            || module.filename.indexOf('dorian') != -1) {  //TODO: stupid hack while the module doesn't come from npm
-            third_party_modules.push(module);
-        } else {
-            internal_modules.push(module);
-        }
-    });
-
-    internal_modules.forEach(function (app_module) {
-        var exported_object = app_module.exports;
-        walkTheTree(exported_object);
-    });
-
-    buildTestSuite();
-
-    incrementalMochaRun(collector, function (failures) {
-        var report = Report.create('html');
-        report.writeReport(collector, true, function () {
-            console.log('Generated report');
-        });
-
-        console.log('Tests ran: %s', mocha.suite.tests.length);
-        console.log('Failures: %s', failures || 0);
-        console.log('Dorian run successful');
-    });
-}
-
-function incrementalMochaRun(collector, callback) {
-    setTimeout(function () {
-        var mocha_instance = mocha,
-            coverage = global.__coverage__ = global.__coverage__ || {};
-        mocha = new mocha_module({
-            ui: 'tdd',
-            reporter: 'spec'
-        });
-
-        test_pass_incrementor++;
-        console.log('Tests generated for pass (%s): %s', test_pass_incrementor, mocha_instance.suite.tests.length);
-        console.log('Starting Mocha test run...');
-
-        mocha_instance.run(function (failures) {
-            collector.add(coverage);
-            buildTestSuite();
-            if (mocha.suite.tests.length > 0) {
-                incrementalMochaRun(collector, callback);
+                        if (assertion && !test_matrix.hasCallback) {
+                            walkTheTree(exported_object.bind(this, test_wired_args));
+                            done();
+                        }
+                    }));
+                });
             } else {
-                callback(failures);
+                mocha.suite.addTest(new mochaTest(fn_declaration[0] + ' - handles - ' + JSON.stringify(fn_args), function (done) {
+                    var test_wired_args = [],
+                        assertion;
+                    if (hasCallback) {
+                        test_wired_args = [callback_fn(done)];
+                    }
+
+                    assertion = assert.doesNotThrow(exported_object.bind(this, test_wired_args), /.*/);
+
+                    if (assertion && !hasCallback) {
+                        walkTheTree(exported_object.bind(this, test_wired_args));
+                        done();
+                    }
+                }));
+            }
+        }
+    },
+    callback_fn: function callback_fn(done) {
+        return function () {
+            if (arguments) {
+                for (var idx = 0; idx < arguments.length; idx++) {
+                    expect(typeof arguments[idx]).to.not.eq('error')
+                        && arguments[idx]
+                        && this.walkTheTree(arguments[idx]);
+                }
+            }
+            done();
+        };
+    },
+    fill_callback_fn: function fill_callback_fn(options, done) {
+        options.matrix.splice(options.callback_arg_position, 0, this.callback_fn(done));
+        return matrix;
+    },
+    generateMatrix: function generateMatrix(fn_args){
+        /**
+         This is used to detect a callback as one of the fn_params
+         But this happens by arg name in a whitelist.  Which is kinda dumb.
+         Do it better.
+         */
+        var callback_arg_position = -1,
+            matrix = {};
+        this.callback_whitelist.forEach(function(list_item){
+            if (callback_arg_position == -1) {
+                callback_arg_position = fn_args.indexOf(list_item);
             }
         });
-    }, 1000);
-}
+        if(callback_arg_position == -1){
+            matrix.values = fn_args.length > 0 && cmbx.baseN(this.arg_test_values, fn_args.length).toArray();
+        } else {
+            matrix = {
+                values: cmbx.baseN(this.arg_test_values, fn_args.length - 1).toArray(),
+                hasCallback: true,
+                callback_arg_position: callback_arg_position,
+                wireCallback: this.fill_callback_fn
+            };
+        }
+        return matrix;
+    },
+    buildTestSuite: function buildTestSuite() {
+        var floor = multi_pass_generator.length;
+        if (test_pass_incrementor == 0 || multi_pass_generator.length < 100){
+            floor = 0;
+        } else {
+            floor = multi_pass_generator.length - 100;
+        }
+        console.log('Building new test suite');
+        while (multi_pass_generator.length > floor) {
+            multi_pass_generator.pop()();
+        }
+    },
+    seeWhatBreaks: function seeWhatBreaks() {
+        module.parent.children.forEach(function (module) {
+            if (module.filename.indexOf('node_modules') == -1
+                || module.filename.indexOf('dorian') == -1) {  //TODO: stupid hack while the module doesn't come from npm
+                internal_modules.push(module);
+            }
+        });
+
+        internal_modules.forEach(function (app_module) {
+            var exported_object = app_module.exports;
+            walkTheTree(exported_object);
+        });
+
+
+
+        buildTestSuite();
+
+        incrementalMochaRun(dorian_ctx.coverage_collector, function (failures) {
+            var report = dorian_ctx.coverage_reporter.create('html');
+            report.writeReport(dorian_ctx.coverage_collector, true, function () {
+                console.log('Generated report');
+            });
+
+            console.log('Tests ran: %s', dorian_ctx.mocha.suite.tests.length);
+            console.log('Failures: %s', failures || 0);
+            console.log('Dorian run successful');
+        });
+    },
+    incrementalMochaRun: function incrementalMochaRun(collector, callback) {
+        setTimeout(function () {
+            var mocha_instance = mocha,
+                coverage = global.__coverage__ = global.__coverage__ || {};
+            mocha = new mocha_module({
+                ui: 'tdd',
+                reporter: 'spec'
+            });
+
+            test_pass_incrementor++;
+            console.log('Tests generated for pass (%s): %s', test_pass_incrementor, mocha_instance.suite.tests.length);
+            console.log('Starting Mocha test run...');
+
+            mocha_instance.run(function (failures) {
+                collector.add(coverage);
+                buildTestSuite();
+                if (mocha.suite.tests.length > 0) {
+                    incrementalMochaRun(collector, callback);
+                } else {
+                    callback(failures);
+                }
+            });
+        }, 1000);
+    }
+};
+
+/**
+ * Dynamic memory allocation
+ * Keep all expando objects in here.  Treat this as the dorian ops RAM disk.
+ *
+ * Optimization for later- attempt to predict an object's upper-bound
+ * memory usage, and pre-allocate at an appropriate size.
+ */
+var dorian_workspace = {
+    internal_modules: [],
+    multi_pass_generator: [],
+    test_pass_incrementor: 0,
+
+};
+
 
 module.exports = {
     run: seeWhatBreaks
